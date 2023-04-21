67c6c1ce77d0c1d22afc76926245cc1d3392fd22
AnupHowlader247 <anuphowlader247@gmail.com>
2023-04-21 21:21:07 +0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,8 @@
 
 function Footer() {
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">
-       Footer
-    </h1>
+    <div className="bg-base-100">
+      <footer><div><div className="text-center text-xl font-semibold"><span>© 2023 Company, <span className="text-info fw-bold">CSE-BOOK</span>. All rights reserved.</span></div></div></footer>
     </div>
   );
 }
