c7a781d372754129ea55180f222dc8213c03d921
AnupHowlader247 <anuphowlader247@gmail.com>
2023-04-21 20:35:54 +0300

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import Router from './Route/Route';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div>
     <RouterProvider router={Router} />
+    <Toaster/>
     </div>
   );
 }
--- src/Page/Login/Login.js
+++ src/Page/Login/Login.js
@@ -1,6 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import toast from 'react-hot-toast';
 
 function Login() {
+  const navigate = useNavigate();
+  const notify = () => toast.error('Something wrong!');
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const info = {
+      email,
+      password
+    }
+
+    fetch('http://localhost:5000/loginUser', {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(info),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data.success){
+        navigate('/home')
+        }
+        else{
+          notify();
+        }
+      });
+  }
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -10,23 +41,26 @@ function Login() {
             <p className="py-6">Description</p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleForm}>
             <div className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
+                <input type="text" placeholder="email" className="input input-bordered" name="email"/>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
+                <input type="text" placeholder="password" className="input input-bordered" name="password"/>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary" type="submit">Login</button>
               </div>
             </div>
+            </form>
+
             <div className="form-control p-6 -mt-10">
               <p>
                 Don't have an account? <Link to={'/register'} className="text-info"> Register</Link>
